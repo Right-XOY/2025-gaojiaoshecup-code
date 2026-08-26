1ee3bcdf2bd08388776d5d0cc59c1806710c324a
Jude Tanson <604605050@qq.com>
2026-08-26 12:41:16 +0800

    图片新增与修复

--- figure/sensitivity_n_convergence.m
+++ figure/sensitivity_n_convergence.m
@@ -1,5 +1,5 @@
-%% sensitivity_n_logX_clean.m
-%% X对数轴：自动放大n=6,12,24,48；后端100‑600压缩；无网格
+%% sensitivity_n_convergence.m
+%% X对数轴：自动放大n=6,12,24,48；后端100-600压缩；无网格
 clear; clc; close all;
 
 n_list  = [6, 12, 24, 48, 100, 200, 300, 400, 600];
@@ -27,17 +27,6 @@ yline(T_inf, '--', ...
     'Color', [0.65, 0.25, 0.25], ...
     'LineWidth', lw_ref);
 
-% n=100标记
-idx_100 = find(n_list == 100);
-semilogx(n_list(idx_100), T_rec(idx_100), 'o', ...
-    'Color', [0, 0.45, 0.70], ...
-    'MarkerFaceColor', [0, 0.45, 0.70], ...
-    'MarkerEdgeColor', 'w', ...
-    'MarkerSize', 9);
-
-text(110, 1.391685, '\it n \rm = 100', ...
-    'FontSize', 9, 'Color', [0, 0.45, 0.70]);
-
 %% 坐标轴设置
 xlabel('圆周采样点数 \it n', 'FontSize', fs);
 ylabel('有效遮蔽时长 \it T \rm (s)', 'FontSize', fs);
@@ -55,8 +44,8 @@ set(gca, 'FontName','Times New Roman');
 set(gca, 'FontSize',fs);
 set(gca, 'Box','on');
 
-legend({'计算遮蔽时长','收敛极限 \it T\rm_\infty = 1.391643 s','模型选用点'},...
+legend({'计算遮蔽时长','收敛极限 \it T\rm_\infty = 1.391643 s'},...
     'Location','southwest','Box','off','FontSize',9);
 
-print(gcf,'sensitivity_n_logX_clean','-dpng','-r300');
+print(gcf,'sensitivity_n_convergence','-dpng','-r300');
 fprintf('输出完成，对数X轴，前几个点自动拉开间距\n');
